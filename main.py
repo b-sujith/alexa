29399266fe0488978b4eaa1a004c6ad07edf8ea7
failur-e <beerasujith@gmail.com>
2021-09-15 17:17:24 +0530

    initial version-say time,play function from YouTube

---
+++ main.py
@@ -0,0 +1,43 @@
+import pyttsx3
+import speech_recognition as spe
+import pywhatkit
+import datetime as dt
+
+listener = spe.Recognizer()
+audio = pyttsx3.init()
+
+# make alexa repeat what u say
+
+
+def talk(speech):
+    audio.say(speech)
+    audio.runAndWait()
+
+
+def take_command():
+    try:
+        with spe.Microphone() as source:
+            listener.adjust_for_ambient_noise(source)
+            print('listening amigo...')
+            voice = listener.listen(source)
+            cmd = listener.recognize_google(voice)
+            if 'mate' in cmd:
+                cmd = cmd.replace('mate', '')
+                print(voice)
+    finally:
+        print('cannot hear u mate')
+    return cmd
+
+
+def run_command():
+    cmd = take_command()
+    if 'play' in cmd:
+        music = cmd.replace('play', '')
+        talk('talking'+cmd)
+        pywhatkit.playonyt(music)
+    elif 'time' in cmd:
+        current_time = dt.datetime.now().strftime('%I:%M:%p')
+        talk(current_time)
+
+
+run_command()
